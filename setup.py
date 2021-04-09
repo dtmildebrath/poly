da01f3ccf144797613156b9c3a2198bc51eb5c03
dmildy <DavidMildebrath1@gmail.com>
2021-04-09 09:19:42 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="poly",
+    version="0.1",
+    description="Simple routines using Gurobi and Qhull to explore facial structure of polytopes",
+    url="https://www.caam.rice.edu/~dtm3/",
+    author="David Mildebrath",
+    author_email="dtm3@rice.edu",
+    packages=["poly"],
+    zip_safe=False,
+)
